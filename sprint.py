979f043c35ec2339ae0e1b131be075363f64ba5c
Luka <luka.z@hotmail.de>
2024-04-24 14:51:28 +0200

    pdf pages get translated individually

--- sprint.py
+++ sprint.py
@@ -1,19 +1,14 @@
-import wave
+import os
+import time
 
 import numpy as np
 import scipy
 import torch
-from pydub import AudioSegment
+from pypdf import PdfReader
 from transformers import AutoModelForTextToWaveform, AutoTokenizer, VitsModel
 
 
-def generate_speech(text):
-    model_name = "facebook/mms-tts-deu"  # Replace with the model you selected
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    # model = VitsModel.from_pretrained(model_name)
-    model = AutoModelForTextToWaveform.from_pretrained(model_name)
-    # Load model directly
-
+def generate_speech(text, model, tokenizer, file_name="test.wav"):
     inputs = tokenizer(text, return_tensors="pt")
     with torch.no_grad():
         output = model(**inputs).waveform
@@ -21,22 +16,28 @@ def generate_speech(text):
 
     # Normalize the audio data to the range [-1, 1]
     output /= np.max(np.abs(output))
-    # Convert waveform data to a compatible data type (e.g., int16)
-    #    output = output.squeeze().cpu().numpy().astype("int16")
 
     # Write the waveform to a WAV file
-    scipy.io.wavfile.write("techno.wav", rate=model.config.sampling_rate, data=output)
+    scipy.io.wavfile.write(file_name, rate=model.config.sampling_rate, data=output)
 
 
-#    scipy.io.wavfile.write("techno.wav", rate=model.config.sampling_rate, data=output)
-# Save the audio
+large_text = ""
+# list all files in the "current_pdf" dir
+pdf_dir = "current_pdf"
+files = os.listdir(pdf_dir)
+pdf_name = files[0]
+reader = PdfReader(pdf_dir + "/" + pdf_name)
+model_name = "facebook/mms-tts-deu"  # Replace with the model you selected
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForTextToWaveform.from_pretrained(model_name)
 
-
-#    with wave.open("output.wav", "wb") as audio_file:
-#        audio_file.setparams((1, 2, 22050, 0, "NONE", "NONE"))
-#        audio_file.writeframes(output.numpy().tobytes())
-
-
-# Example usage
-text_to_speak = "Meine Schwester Elena stinkt"
-generate_speech(text_to_speak)
+for page in reader.pages[3:91]:
+    print(f"Start Page {page.page_number}")
+    start_time = time.time()
+    generate_speech(
+        page.extract_text(),
+        model,
+        tokenizer,
+        f"output/{pdf_name[:-4]}_{page.page_number}.wav",
+    )
+    print(f"Page {page.page_number} took {time.time()-start_time} seconds")
